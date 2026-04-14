217d19444da2d222dc59b147a31b449e2f48f78c
Brandon Wong <bwong1086@sdsu.edu>
2026-04-13 23:56:47 -0700

    Added doctest to my methods.

--- Haynes_OOP_FinalProj_2026.py
+++ Haynes_OOP_FinalProj_2026.py
@@ -99,6 +99,16 @@ class Seq:
     def __str__(self):
         return self.sequence
 
+    def __len__(self): #Adds len overload so we can access the seq length of the object - Brandon Wong
+        """Len overload to return the length of the sequence from the Seq class.
+
+        >>> s=Seq("AGTAGC","tmp","m")
+        >>> len(s)
+        6
+        """
+
+        return len(self.sequence)
+
     # adds eq overload to compare two seq objects
     def __eq__(self,other):
         return self.sequence == other.sequence
@@ -205,5 +215,19 @@ class Protein(Seq):
         for aa in self.sequence:
             weight += aa_mol_weights[aa]
         return weight
+
+    def amino_acid_composition(self): #Added aa comp dictionary - Brandon Wong
+        """Creates a dictionary of the amino acids and their count in the sequence.
+        >>> r=Protein("AGTXAGC","tmp","m","geneid_test")
+        >>> r.amino_acid_composition()
+        {'A': 2, 'G': 2, 'T': 1, 'X': 1, 'C': 1}
+        """
+        aa_composition={}
+        for aa in self.sequence:
+            if aa in aa_composition:
+                aa_composition[aa] += 1
+            else:
+                aa_composition[aa] = 1
+        return aa_composition
 
 x=DNA("G","tmp","m",000)
